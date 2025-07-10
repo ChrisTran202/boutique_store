4ffa0f4e5287b7283d23eebb082f5e76d8344f4d
[Chris Tran] <christran202@gmail.com>
2025-07-10 16:01:30 -0500

    Images redirected

--- components/ItemsData.tsx
+++ components/ItemsData.tsx
@@ -5,63 +5,63 @@ export const itemsData = [
   {
     id: 1,
     title: 'Golden Dress',
-    img: '/images/outfit/8.png',
+    img: '/images/8.png',
     description: '',
     
   },
   {
     id: 2,
     title: 'Yellow Dress',
-    img: '/images/outfit/1.png',
+    img: '/images/1.png',
     description: '',
     
   },
   {
     id: 3,
     title: 'Causual ',
-    img: '/images/outfit/5.png',
+    img: '/images/5.png',
     description: '',
     
   },
   {
     id: 4,
     title: 'Black Top and Jeans',
-    img: '/images/outfit/2.png',
+    img: '/images/2.png',
     description: '',
     
   },
   {
     id: 5,
     title: 'Forest Green Dress',
-    img: '/images/outfit/3.png',
+    img: '/images/3.png',
     description: '',
     
   },
   {
     id: 6,
     title: 'Summner Tank Brown shorts',
-    img: '/images/outfit/4.png',
+    img: '/images/4.png',
     description: '',
     
   },
   {
     id: 7,
     title: 'Business',
-    img: '/images/outfit/6.png',
+    img: '/images/6.png',
     description: '',
     
   },
   {
     id: 8,
     title: 'Floral Purple',
-    img: '/images/outfit/7.png',
+    img: '/images/7.png',
     description: '',
     
   },
   {
     id: 9,
     title: 'All White Party',
-    img: '/images/outfit/9.png',
+    img: '/images/9.png',
     description: '',
     
   },
